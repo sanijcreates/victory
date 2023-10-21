05c95324344226e98dd1781291e2934368050a72
Sanij Shrestha <saniz.sth123@gmail.com>
2023-10-21 17:17:02 -0400

    added ingredients option

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
+import SignInPage from './signin/page'
+import app from "../../firebase";
+import Link from 'next/link';
 
-export default function Home() {
-  // const onClickHandler=async()=>{
-  //   const data=await axios.post("http://localhost:3000/api/terra")
-  //   console.log(data)
-  // }
-  return (
-    <main className="">
-
-    </main>
-  )
-}
+  export default function Home() {
+    return (
+      <main className="h-screen flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-bold mt-10">Welcome!</h1>
+        <h2 className="text-lg text-gray-600 mt-4">Get started</h2>
+        <div className="mt-8">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mr-4"><Link href ="/signin">Sign In</Link></button>
+          <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"><Link href ="/signup">Sign Up</Link></button>
+        </div>
+      </main>
+    )
+  }
