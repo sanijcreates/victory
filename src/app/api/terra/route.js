5f8818fd1e5e69169f7c0d38b2f9aae17a1854a0
PrabeshPP <prabesh.bista369@gmail.com>
2023-10-21 17:19:12 -0400

    added

--- src/app/api/terra/route.js
+++ src/app/api/terra/route.js
@@ -3,9 +3,10 @@ import Terra from "terra-api";
 
 const terra = new Terra("teamvictory-testing-MVNMTVakEO", "P9cNY2JqfTZkwn-xVv3Y6CSVyC1BwFY2","mongodb+srv://cluster0.yqef2wq.mongodb.net");
 export async function POST(request) {
-    const data = await terra.getNutrition({userId:"5d4556b2-85ab-4fe4-b606-694ef95be4e9",startDate: new Date("2023-03-10"), endDate: new Date(), toWebhook: false })
+    const data = await terra.getNutrition({userId:"6799b7d8-a857-4783-98bd-136e38b3621c",startDate: new Date("2023-03-10"), endDate: new Date(), toWebhook: false })
 
     // Handle data here
+    console.log("Hello World")
     console.log(data)
     return NextResponse.json({ data: data }, { status: 200}); 
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,21 @@
+'use client'
+import { ConnectToTerraButton } from '@tryterra/terra-ui'
+import axios from 'axios';
+import SignInPage from './signin/page'
+import app from "../../firebase";
 
 export default function Home() {
-  // const onClickHandler=async()=>{
-  //   const data=await axios.post("http://localhost:3000/api/terra")
-  //   console.log(data)
-  // }
+  const onClickHandler=async()=>{
+    const data=await axios.post("http://localhost:3000/api/terra")
+    console.log(data)
+  }
   return (
     <main className="">
+    <button onClick={onClickHandler}>Click Me</button>
+      <ConnectToTerraButton onClick={async () => {
+        const url = (await axios.get("http://localhost:3000/api/generateWidgetSession")).data.url 
+        window.location = url
+      }} />
 
     </main>
   )
