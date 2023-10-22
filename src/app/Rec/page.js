67898a0a2222aeea6af3aba661f7361a960555ba
PrabeshPP <prabesh.bista369@gmail.com>
2023-10-22 03:41:55 -0400

    added

--- src/app/Rec/page.js
+++ src/app/Rec/page.js
@@ -1,63 +1,66 @@
 'use client'
 import { useSearchParams } from 'next/navigation'
 
-import React,{useCallback, useEffect, useState} from "react";
-import { auth,db } from "../../../firebase";
-import { collection, doc,getDocs } from "firebase/firestore"; 
+import React, { useCallback, useEffect, useState } from "react";
+import { auth, db } from "../../../firebase";
+import { collection, doc, getDocs } from "firebase/firestore";
+import axios from 'axios';
 
 
 
 function Page(props) {
-    const searchParams = useSearchParams()
-    const [food, setFood] = useState('')
-    const sleep = searchParams.get('sleep')
-    const heartRate = searchParams.get('heartRate')
-    const energy = searchParams.get('energy')
-    const cal = searchParams.get('cal')
-    const steps = searchParams.get('steps')
+  const searchParams = useSearchParams()
+  const [food, setFood] = useState('')
+  const [result,setResult]=useState("")
+  const sleep = searchParams.get('sleep')
+  const heartRate = searchParams.get('heartRate')
+  const energy = searchParams.get('energy')
+  const cal = searchParams.get('cal')
+  const steps = searchParams.get('steps')
 
-    const prompt = `I normally for ${sleep} seconds, my avg heart rate is ${heartRate} bpm, my net activity calories is ${cal}, my average steps in a day are ${steps}, I have ${food} in my kitchen. In regard to to improve my sleep, heart rate, calorie intake, steps, what could be a day of diet for me that could improve my health. answer it in bullet points starting from 1. Give suggestions like a doctor. Include the data provided by me in your answers. Do not include any other information. start your answer from bullet points`
+  let prompt = ""
 
-    useEffect (() => {
-      
-        const unsubscribe = auth.onAuthStateChanged((authUser) => {
-        //   setIsLoading(true)
-          if (authUser) {
-            // User is signed in
+  const fetchRecommendation=useCallback(async(prompt)=>{
+    const response=await axios.post("http://localhost:3000/api/recommendations",{"prompt":prompt});
+    const data=response.data
+    setResult(data.data)
+})
 
-            fetchUserData(authUser.uid)
-          } else {
-            // User is signed out
-            // setUser(null);
-          }
-        });
-        // Unsubscribe the listener when the component unmounts
-        return () => {
-          unsubscribe();
-        };
-    },[])
-    const fetchUserData=useCallback(async(uid)=>{
-        // const querySnapShot=await db.collection(uid).get();
-        const snapshot=await getDocs(collection(db,uid));
-        const data = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        console.log(data[0]);
-        setFood(data[0].ingredients)
-        console.log(data[0].ingredients);  
-      })
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      //   setIsLoading(true)
+      if (authUser) {
+        // User is signed in
 
-  console.log(searchParams.get('sleep'));
-  console.log(searchParams.get('heartRate'));
-  console.log(searchParams.get('cal'));
-  console.log(searchParams.get('steps'));
-  console.log(searchParams.get('uid'));
-    return (// Inside your sign-up component
+        fetchUserData(authUser.uid)
+      } else {
+        // User is signed out
+        // setUser(null);
+      }
+    });
+    // Unsubscribe the listener when the component unmounts
+    return () => {
+      unsubscribe();
+    };
+  }, [])
+  const fetchUserData = useCallback(async (uid) => {
+    // const querySnapShot=await db.collection(uid).get();
+    const snapshot = await getDocs(collection(db, uid));
+    const data = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    prompt = `I normally slept for ${sleep} seconds, my avg heart rate is ${heartRate} bpm, my net activity calories is ${cal}, my average steps in a day are ${steps}, I have ${data[0].ingredients} in my kitchen. In regard to to improve my sleep, heart rate, calorie intake, steps, what could be a day of diet for me that could improve my health. answer it in bullet points starting from 1. Give suggestions like a doctor. Include the data provided by me in your answers. Do not include any other information. start your answer from bullet points`
+    fetchRecommendation(prompt)
+    setFood(data[0].ingredients)
+  })
+
+
+  return (// Inside your sign-up component
     <div className="flex justify-center items-center h-screen">
-        <h1>as</h1>
-</div>
-    );
+      {result.length==0?<div className='inline-block h-[100px] w-[100px] animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]'></div>:<div className=' whitespace-pre-line'>{result}</div>}
+    </div>
+  );
 }
 
 export default Page;
