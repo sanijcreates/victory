c79df4a5ae518f1ba1b4388ab7930ee180ccd158
Sanij Shrestha <saniz.sth123@gmail.com>
2023-10-22 01:04:17 -0400

    ok

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,34 @@
 'use client'
-import { auth } from "../../firebase"
-import { useState, useEffect } from "react";
+import {auth} from "../../firebase"
+import { useState,useEffect } from "react";
 import Link from "next/link";
-import Profile from "./profile/[uid]/page";
-import HealthPage from "./HealthData/[uid]/page";
-
 
 export default function Home() {
   const [user, setUser] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [results, setResults] = useState([])
+  const user_id = '6799b7d8-a857-4783-98bd-136e38b3621';
+  const user_input = 'What can i do to lose 10kgs in a month?';
+  const headers = {
+    'dev-id': 'teamvictory-testing-MVNMTVakEO',
+    'api-key': 'P9cNY2JqfTZkwn-xVv3Y6CSVyC1BwFY2', 
+  };
 
   useEffect(() => {
+   async function generatePrompts(engine, prompt) {
+    const response = await openai.createCompletion({
+      engine: engine,
+      prompt: prompt,
+      max_tokens: 1024,
+      temperature: 0.5
+    });
+    return response.choices[0].text.trim();
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
-      setIsLoading(true)
       if (authUser) {
         // User is signed in
-        console.log(authUser.uid)
         setUser(authUser);
-        setIsLoading(false)
       } else {
         // User is signed out
         setUser(null);
-        setIsLoading(false)
       }
     });
 
@@ -33,23 +40,17 @@ export default function Home() {
 
 
   return (
-    <main className="h-screen w-[100%]flex flex-col justify-center items-center">
-      {isLoading? <div className=" h-[100vh] w-[100%]">
-        <div className="h-[100px] w-[100px] bg-red-500 animate-spin rounded-full">
-
-        </div>
-
-      </div> : user ?
-        <>
-          <HealthPage />
-        </>
-        : <><h1 className="text-4xl font-bold mt-10">Welcome!</h1>
-          <h2 className="text-lg text-gray-600 mt-4">Get started</h2>
-          <div className="mt-8">
-            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mr-4"><Link href="/signin">Sign In</Link></button>
-            <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"><Link href="/signup">Sign Up</Link></button>
-          </div> </>
-      }
-    </main>
-  )
-}
+    <main className="h-screen flex flex-col justify-center items-center">
+    {user ? 
+    <>
+    <h1> Show profile page here</h1>
+    </>
+      :  <><h1 className="text-4xl font-bold mt-10">Welcome!</h1>
+        <h2 className="text-lg text-gray-600 mt-4">Get started</h2>
+        <div className="mt-8">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mr-4"><Link href ="/signin">Sign In</Link></button>
+          <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"><Link href ="/signup">Sign Up</Link></button>
+        </div> </>
+  }
+        </main>
+  )}  
