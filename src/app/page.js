cd0c6400bfdcfdad4fa14f72c371a6c18c64ce13
Sanij Shrestha <saniz.sth123@gmail.com>
2023-10-21 22:31:54 -0400

    added homepage//needs changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 'use client'
 import {auth} from "../../firebase"
 import { useState,useEffect } from "react";
+import Link from "next/link";
+
 export default function Home() {
   const [user, setUser] = useState(null);
 
@@ -23,8 +25,17 @@ export default function Home() {
 
 
   return (
-    <main className="">
-      {user?<h1>User</h1>:<h1>Not User</h1>}
-    </main>
-  )
-}
+    <main className="h-screen flex flex-col justify-center items-center">
+    {user ? 
+    <>
+    <h1> Show profile page here</h1>
+    </>
+      :  <><h1 className="text-4xl font-bold mt-10">Welcome!</h1>
+        <h2 className="text-lg text-gray-600 mt-4">Get started</h2>
+        <div className="mt-8">
+          <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mr-4"><Link href ="/signin">Sign In</Link></button>
+          <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"><Link href ="/signup">Sign Up</Link></button>
+        </div> </>
+  }
+        </main>
+  )}
